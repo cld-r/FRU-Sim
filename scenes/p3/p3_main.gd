cc6485c90ae80b5e111ea1696466bdb4ffe17e0b
Will Craycroft <wcraycroft@gmail.com>
2024-12-10 21:13:33 -0500

    Initial Commit.

--- scenes/p3/p3_main.gd
+++
@@ -1,9 +0,0 @@
-# Copyright 2025 by William Craycroft
-# All rights reserved.
-# This file is released under "GNU General Public License 3.0".
-# Please see the LICENSE file that should have been included as part of this package.
-
-## This is a basic template for an encounter "phase". It can support multiple sequences
-## that can be run back to back, or started from a specific point.
-
-extends Sequence
--- scenes/templates/sequence/seq_1.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-func start_sequence(new_party: Dictionary) -> void:
-	assert(new_party != null, "Error. Where the party at?")
